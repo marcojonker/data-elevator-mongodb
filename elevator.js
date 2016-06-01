129297798f845104546df3414e701ea30786c5ab
Marco <mj.jonker@live.nl>
2016-06-01 22:26:07 +0200

    Bugfixes

--- elevator.js
+++ elevator.js
@@ -5,9 +5,9 @@
 
 'use strict'
 
-var ElevatorBase = require('data-elevator/elevator-base');
-var ConsoleLogger = require('data-elevator/logger/console-logger');
-var LevelController = require('./lib/controllers/level-controllers/mongodb-level-controller.js');
+var ElevatorBase = require('data-elevator/lib/elevator-engine/elevator-base');
+var ConsoleLogger = require('data-elevator/lib/logger/console-logger');
+var LevelController = require('./lib/level-controllers/mongodb-level-controller.js');
 
 //__dirname is added only for the construct command so construct command knows where to find its resources
 var elevator = new ElevatorBase(new ConsoleLogger(false), LevelController, __dirname);
