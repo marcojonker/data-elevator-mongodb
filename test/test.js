8720723bf85696b0eedf3f068ae8ed60408a84bb
Marco <mj.jonker@live.nl>
2016-10-18 23:04:31 +0200

    add comment

--- test/test.js
+++ test/test.js
@@ -8,5 +8,6 @@ var TestBase = require('../node_modules/data-elevator/test/test-base.js');
 var path = require('path');
 var MongoDbLevelController = require('../lib/level-controllers/mongodb-level-controller.js');
 
+//Start testing
 var test = new TestBase(path.normalize(path.join(__dirname, '../')), MongoDbLevelController);
 test.runDefaultCommandTests();
