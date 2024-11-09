414861020f3a794b9a496f506dc67f5e996f978e
marcojonker <mj.jonker@live.nl>
2024-11-09 13:37:31 +0100

    Update data-elevator-test-config.js

--- test/resources/data-elevator-test-config.js
+++ test/resources/data-elevator-test-config.js
@@ -1,7 +1,7 @@
 var config = {
     levelControllerConfig: {
        collectionName: "_data_elevator",
-       connectionOptions: { useNewUrlParser: true},
+     //  connectionOptions: { useNewUrlParser: true},
        connectionUrl: "mongodb://localhost:27017/admin"
     }
 }
